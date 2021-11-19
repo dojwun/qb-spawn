c1a153794160f8aa7721e7041fe738c2a4aff389
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-18 17:48:10 -0800

    updated to vector4

--- config.lua
+++ config.lua
@@ -2,82 +2,42 @@ QB = {}
 
 QB.Spawns = {
     ["legion"] = {
-        coords = { 
-            x = 202.331, 
-            y = -931.175, 
-            z = 30.692, 
-            h = 198.452 
-    },
+        coords = vector4(195.17, -933.77, 29.7, 144.5),
         location = "legion",
         label = "Legion Square",
     },
     ["paleto"] = {
-        coords = {
-            x = 80.35, 
-            y = 6424.12, 
-            z = 31.67, 
-            h = 45.5 
-    },
+        coords = vector4(80.35, 6424.12, 31.67, 45.5),
         location = "paleto",
         label = "Paleto Bay",
     },
     ["motel"] = {
-        coords = {
-            x = 326.29, 
-            y = -212.31, 
-            z = 55.08, 
-            h = 282.91 
-    }, 
+        coords = vector4(327.56, -205.08, 53.08, 163.5),
         location = "motel",
         label = "Pinkcage Motel",
     },
     ["bus"] = {
-        coords = { 
-        x = 454.349, 
-        y = -661.036, 
-        z = 27.6534, 
-        h = 282.91
-    }, 
+        coords = vector4(454.349, -661.036, 27.6534, 282.91),
         location = "bus",
         label = "Dashound Bus Center",
     },
     ["train"] = {
-        coords = { 
-        x = -206.674, 
-        y = -1015.1, 
-        z = 30.1381, 
-        h = 282.91 
-    }, 
+        coords = vector4(-206.674, -1015.1, 30.1381, 282.91),
         location = "train",
         label = "Altee St. Train Station",
     },
     ["sandy"] = {
-        coords = { 
-        x = 1955.54, 
-        y = 3843.48, 
-        z = 32.0165, 
-        h = 282.91
-    }, 
+        coords = vector4(1955.54, 3843.48, 32.0165, 282.91),
         location = "sandy",
         label = "Sandy Shores",
     },
     ["pier"] = {
-        coords = { 
-        x = -1686.61, 
-        y = -1068.16, 
-        z = 13.1522, 
-        h = 282.91 
-    }, 
+        coords = vector4(-1686.61, -1068.16, 13.1522, 282.91),
         location = "pier",
         label = "Del Perro Pier",
     },
     ["vinewood"] = {
-        coords = { 
-        x = 280.571, 
-        y = 182.679, 
-        z = 104.504, 
-        h = 282.91 
-    }, 
+        coords = vector4(280.571, 182.679, 104.504, 282.91),
         location = "vinewood",
         label = "Vinewood",
     },
